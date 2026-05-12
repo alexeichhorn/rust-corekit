f2303fd3ea614f1ed5cd70ff46d268aca957e585
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-05-12 19:48:50 +0200

    implement secret string

--- crates/corekit/src/lib.rs
+++ crates/corekit/src/lib.rs
@@ -1,6 +1,9 @@
 //! Small Rust ergonomics library for backend projects.
 
+mod secret_string;
+
 pub use corekit_macros::*;
+pub use secret_string::SecretString;
 
 pub mod prelude {
     pub use crate::*;
---
+++ crates/corekit/src/secret_string.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+#[derive(Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct SecretString(String);
+
+impl SecretString {
+    pub fn expose(&self) -> &str {
+        &self.0
+    }
+}
+
+impl fmt::Debug for SecretString {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str("SecretString([redacted])")
+    }
+}
+
+impl fmt::Display for SecretString {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str("[redacted]")
+    }
+}
+
+impl From<String> for SecretString {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl From<&str> for SecretString {
+    fn from(value: &str) -> Self {
+        Self(value.to_owned())
+    }
+}
